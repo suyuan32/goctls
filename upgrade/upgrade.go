e9dc0b22b369ae93a44f29c4f1e9d7e53a6e397f
Ryan SU <yuansu.china.work@gmail.com>
2023-07-28 08:25:30 +0800

    fix: optimize upgrade command

--- upgrade/upgrade.go
+++ upgrade/upgrade.go
@@ -2,8 +2,6 @@ package upgrade
 
 import (
 	"fmt"
-	"runtime"
-
 	"github.com/spf13/cobra"
 	"github.com/suyuan32/goctls/rpc/execx"
 )
@@ -11,10 +9,7 @@ import (
 // upgrade gets the latest goctl by
 // go install github.com/suyuan32/goctls@latest
 func upgrade(_ *cobra.Command, _ []string) error {
-	cmd := `GO111MODULE=on GOPROXY=https://goproxy.cn/,direct go install github.com/suyuan32/goctls@latest`
-	if runtime.GOOS == "windows" {
-		cmd = `set GOPROXY=https://goproxy.cn,direct && go install github.com/suyuan32/goctls@latest`
-	}
+	cmd := `go install github.com/suyuan32/goctls@latest`
 	info, err := execx.Run(cmd, "")
 	if err != nil {
 		return err
