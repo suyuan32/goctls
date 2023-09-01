1f90656cd7369908c95a1040eceb051bf0833265
Ryan SU <yuansu.china.work@gmail.com>
2023-09-01 09:21:20 +0800

    fix: optimize official deps upgrade codes

--- project/upgrade/dependency.go
+++ project/upgrade/dependency.go
@@ -8,35 +8,24 @@ import (
 	"strings"
 )
 
-func upgradeDependencies(workdir string) error {
+func upgradeDependencies(workDir string) error {
 	// drop old replace
 	oldVersion := []string{"v1.5.2", "v1.5.3", "v1.5.4"}
 	for _, v := range oldVersion {
-		_, err := execx.Run(fmt.Sprintf("go mod edit -dropreplace github.com/zeromicro/go-zero@%s", v), workdir)
+		_, err := execx.Run(fmt.Sprintf("go mod edit -dropreplace github.com/zeromicro/go-zero@%s", v), workDir)
 		if err != nil {
 			return errors.New("failed to drop old replace")
 		}
 	}
 
-	data, err := execx.Run("go list -json", workdir)
+	data, err := execx.Run("go list -json", workDir)
 	if err != nil {
 		return err
 	}
 
-	if strings.Contains(data, "simple-admin-common") {
-		_, err = execx.Run(fmt.Sprintf("go mod edit -require=github.com/suyuan32/simple-admin-common@%s",
-			config.CoreVersion), workdir)
-		if err != nil {
-			return err
-		}
-	}
-
-	if strings.Contains(data, "simple-admin-core") {
-		_, err = execx.Run(fmt.Sprintf("go mod edit -require=github.com/suyuan32/simple-admin-core@%s",
-			config.CoreVersion), workdir)
-		if err != nil {
-			return err
-		}
+	err = upgradeOfficialDependencies(data, workDir)
+	if err != nil {
+		return err
 	}
 
 	err = tidy()
@@ -45,4 +34,36 @@ func upgradeDependencies(workdir string) error {
 	}
 
 	return nil
+}
+
+func upgradeOfficialDependencies(data, workDir string) (err error) {
+	deps := []struct {
+		Name string
+		Repo string
+	}{
+		{
+			Name: "simple-admin-common",
+			Repo: "github.com/suyuan32/simple-admin-common",
+		},
+		{
+			Name: "simple-admin-core",
+			Repo: "github.com/suyuan32/simple-admin-core",
+		},
+		{
+			Name: "simple-admin-message-center",
+			Repo: "github.com/suyuan32/simple-admin-message-center",
+		},
+	}
+
+	for _, v := range deps {
+		if strings.Contains(data, v.Name) {
+			_, err = execx.Run(fmt.Sprintf("go mod edit -require=%s@%s", v.Repo,
+				config.CoreVersion), workDir)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return err
 }
