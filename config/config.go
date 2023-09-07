baeb177a481d7cc721d0990266a14fc4d8185b17
Ryan Su <yuansu.china.work@gmail.com>
2023-09-07 11:13:18 +0800

    chore: update default version

--- config/config.go
+++ config/config.go
@@ -15,7 +15,7 @@ const DefaultGoZeroVersion = "v1.5.5"
 const DefaultToolVersion = "v1.5.16"
 
 // GoctlsVersion is goctls version
-const GoctlsVersion = "v1.6.0"
+const GoctlsVersion = "v1.6.1"
 
 // CoreVersion is the core service version.
 const CoreVersion = "v1.1.3"
