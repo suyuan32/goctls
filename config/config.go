d83be3d55ad61c363df82208bc6578a4e4095298
Ryan Su <yuansu.china.work@gmail.com>
2023-10-06 10:10:06 +0800

    fix: add old go zero version config

--- config/config.go
+++ config/config.go
@@ -11,6 +11,9 @@ const DefaultFormat = "go_zero"
 // DefaultGoZeroVersion defines the default version of go zero for migrate
 const DefaultGoZeroVersion = "v1.5.6"
 
+// OldGoZeroVersion defines the old version of go zero for migrate
+var OldGoZeroVersion = []string{"v1.5.2", "v1.5.3", "v1.5.4", "v1.5.5"}
+
 // DefaultToolVersion defines the default version of simple admin tools for migrate
 const DefaultToolVersion = "v1.5.17"
 
