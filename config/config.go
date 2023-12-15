e14fcb4a3225a783b68852021f62cfc72f6a7e1d
Ryan Su <yuansu.china.work@gmail.com>
2023-12-15 14:24:33 +0800

    chore: update version

--- config/config.go
+++ config/config.go
@@ -15,13 +15,13 @@ const DefaultGoZeroVersion = "v1.6.0"
 var OldGoZeroVersion = []string{"v1.5.2", "v1.5.3", "v1.5.4", "v1.5.5", "v1.5.6"}
 
 // DefaultToolVersion defines the default version of simple admin tools for migrate
-const DefaultToolVersion = "v1.6.0"
+const DefaultToolVersion = "v1.6.2"
 
 // GoctlsVersion is goctls version
-const GoctlsVersion = "v1.6.15"
+const GoctlsVersion = "v1.6.16"
 
 // CoreVersion is the core service version.
-const CoreVersion = "v1.2.4"
+const CoreVersion = "v1.2.5"
 
 // LangEnvKey is the environment variable name to control the tools help info language
 const LangEnvKey = "SIMPLE_LANG"
