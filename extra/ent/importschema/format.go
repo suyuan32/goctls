8e97eb18eb8b34705982ddb58881d55bd5ef918d
Ryan Su <yuansu.china.work@gmail.com>
2024-07-01 11:25:10 +0800

    fix: optimize ent import command

--- extra/ent/importschema/format.go
+++ extra/ent/importschema/format.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/duke-git/lancet/v2/fileutil"
+	"github.com/duke-git/lancet/v2/strutil"
 	"github.com/suyuan32/goctls/rpc/execx"
 	"path/filepath"
 	"strings"
@@ -31,9 +32,6 @@ func FormatFile(ctx *GenContext) error {
 		}
 
 		if !strings.Contains(fileStr, "WithComments") && strings.Contains(fileStr, "Comment") {
-			fileStr = strings.ReplaceAll(fileStr, ".Comment",
-				".\n\t\tAnnotations(entsql.WithComments(true)).\n\t\tComment")
-
 			if !strings.Contains(fileStr, "dialect/entsql") {
 				importIndex := strings.Index(fileStr, "import (")
 				fileStr = fileStr[:importIndex+8] + "\n\t\"entgo.io/ent/dialect/entsql\"" + fileStr[importIndex+8:]
@@ -44,6 +42,17 @@ func FormatFile(ctx *GenContext) error {
 			fileStr = strings.ReplaceAll(fileStr, "uuid \"github.com/satori/go.uuid\"",
 				"\"github.com/gofrs/uuid/v5\"")
 		}
+
+		fileStr = removeOldAnnotation(fileStr, getSchemaName(fileStr))
+
+		fileStr += fmt.Sprintf(`
+
+func (%s) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.WithComments(true),
+		entsql.Annotation{Table: "%ss"},
+	}
+}`, getSchemaName(fileStr), strings.ToLower(strutil.SnakeCase(getSchemaName(fileStr))))
 
 		if err := fileutil.WriteStringToFile(filePath, fileStr, false); err != nil {
 			return err
@@ -57,4 +66,30 @@ func FormatFile(ctx *GenContext) error {
 	}
 
 	return nil
+}
+
+func getSchemaName(data string) string {
+	typeIndex := strings.Index(data, "type")
+	structIndex := strings.Index(data, "struct")
+
+	return strings.Trim(data[typeIndex+4:structIndex], " ")
+}
+
+func removeOldAnnotation(data, schemaName string) string {
+	lastFuncIndex := strings.LastIndex(data, fmt.Sprintf("func (%s) Annotations()", schemaName))
+	funcEndIndex := 0
+	count := 0
+	for i := lastFuncIndex; i < len(data); i++ {
+		if data[i] == '{' {
+			count++
+		} else if data[i] == '}' {
+			count--
+
+			if count == 0 {
+				funcEndIndex = i + 1
+				break
+			}
+		}
+	}
+	return data[:lastFuncIndex] + data[funcEndIndex:]
 }
