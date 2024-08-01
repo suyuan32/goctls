c247385d9c8ade1bf20122bf5bac21ad72f631c5
Ryan Su <yuansu.china.work@gmail.com>
2024-08-01 09:20:21 +0800

    docs: optimize copyright

--- extra/ent/schema/gen.go
+++ extra/ent/schema/gen.go
@@ -1,15 +1,31 @@
+// Copyright (C) 2023  Ryan SU (https://github.com/suyuan32)
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package schema
 
 import (
 	_ "embed"
 	"errors"
+	"path/filepath"
+	"strings"
+	"text/template"
+
 	"github.com/duke-git/lancet/v2/fileutil"
 	"github.com/gookit/color"
 	"github.com/spf13/cobra"
 	"github.com/suyuan32/goctls/util/format"
-	"path/filepath"
-	"strings"
-	"text/template"
 )
 
 var (
