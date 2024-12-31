603c535da369fbbe11d6c8cf4a04fc0d7d69ec29
Ryan Su <yuansu.china.work@gmail.com>
2024-12-31 10:50:46 +0800

    chore: upgrade version

--- config/config.go
+++ config/config.go
@@ -18,7 +18,7 @@ var OldGoZeroVersion = []string{"v1.6.0", "v1.6.1", "v1.6.2", "v1.6.3", "v1.6.4"
 const DefaultToolVersion = "v1.7.4"
 
 // GoctlsVersion is goctls version
-const GoctlsVersion = "v1.9.3"
+const GoctlsVersion = "v1.9.4"
 
 // CoreVersion is the core service version.
 const CoreVersion = "v1.6.0"
